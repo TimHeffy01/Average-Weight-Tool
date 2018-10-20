a91854b3cd48316e8fd64c8133411f16c5e12256
TimHeffy01 <43121568+TimHeffy01@users.noreply.github.com>
2018-10-20 23:46:47 +0100

    Add files via upload
    custom inputs

--- scripts.js
+++ scripts.js
@@ -1,41 +1,42 @@
-var input  = document.getElementsByTagName('input'),
+var input  = document.getElementsByClassName('num'),
     button = document.getElementsByTagName('button'),
-    output = document.getElementsByClassName('output');
+    output = document.getElementsByClassName('output'),
+    custom = document.getElementsByClassName('custom-weight'),
+    custB  = document.getElementsByClassName('b'),
+    inputs = document.getElementsByTagName('input');
 
 button[0].addEventListener("click", function() {
-	
 	var items  = 0,
 	    weight = 0;
-	    
+	
+	for(var x = 0; x < custom.length; x++) {
+		custB[x].name = custom[x].valueAsNumber;
+	}
 	for(var i = 0; i < input.length; i++) {
 		items  += input[i].valueAsNumber;
 		weight += input[i].valueAsNumber * input[i].name;
 	}
 
 	var averageWeight = Math.floor(weight / items);
-
+	
 	output[0].innerHTML = items;
 	output[1].innerHTML = averageWeight;
-
+	
 	if(items === 0) {
 		alert("Error: No numbers entered.");
 		output[1].innerHTML = 0;
 	}
-
 })
 
 button[1].addEventListener("click", function() {
-	
 	var c = confirm("Are you sure you want to clear all inputs?");
-
 	if (c === true) {
-		for(var i = 0; i < input.length; i++) {
-			input[i].valueAsNumber = 0;
+		for(var i = 0; i < inputs.length; i++) {
+			inputs[i].valueAsNumber = 0;
 		}
 		for(var x = 0; x < output.length; x++) {
 			output[x].innerHTML = 0;
 		}
-	}
-	
+	}	
 })
 
